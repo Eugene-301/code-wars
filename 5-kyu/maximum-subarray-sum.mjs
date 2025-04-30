1281c52d98a9a84ed1ad2998c28251ff087bb2b8
Eugene-301 <ablepiha@duck.com>
2025-04-30 19:56:02 +0300

    First 5-kyu

--- 5-kyu/maximum-subarray-sum.mjs
+++ 5-kyu/maximum-subarray-sum.mjs
@@ -1,13 +1,28 @@
 import test from "../test.mjs";
 
 var maxSequence = function (arr) {
-  if (arr.length === 0) return 0;
-  let sequences = [];
+  if (arr.lenght === 0) return 0;
 
-  arr.forEach((element) => {
-    console.log(element);
-  });
+  let max = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) continue;
+    if (arr[i] > max) max = arr[i];
+
+    let nextSum = arr[i] + arr[i + 1];
+    let offset = 2;
+
+    while (nextSum >= 0 || nextSum) {
+      if (nextSum > max) {
+        max = nextSum;
+      }
+      nextSum += arr[i + offset];
+      offset++;
+    }
+  }
+
+  return max;
 };
 
 test(maxSequence([]), 0);
-test(maxSequence([[-2, 1, -3, 4, -1, 2, 1, -5, 4]]), 6);
+test(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
